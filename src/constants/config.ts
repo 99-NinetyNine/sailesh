40bec1e606935252ae374f52cb72a02b6fb51f7d
bond <you@example.com>
2024-04-30 10:58:36 +0545

    quick deploy

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -40,13 +40,13 @@ type TConfig = {
 
 export const config: TConfig = {
   html: {
-    title: "John Doe — 3D Portfolio",
-    fullName: "John Doe",
-    email: "johndoe@mail.com",
+    title: "Sailesh Shiwakoti",
+    fullName: "Sailesh Shiwakoti",
+    email: "saileshshiwakoti2@gmail.com",
   },
   hero: {
     name: "John Doe",
-    p: ["I develop 3D visuals, user", "interfaces and web applications"],
+    p: ["I develop: Web application", "AI/ML and Blockchain applications"],
   },
   contact: {
     p: "Get in touch",
